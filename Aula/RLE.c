b606913236081c534fef6dc7ea33d9d1723472a9
Matheus <matheusgamer488@icloud.com>
2026-08-25 10:00:27 -0300

    feat: rle exercise

--- Aula/RLE.c
+++ Aula/RLE.c
@@ -1,36 +1,50 @@
 #include <stdio.h>
 
-int contagemRLE(int v[]){
-    int contagem = 1;
-    int nAtual = v[0];
-    int len = sizeof(v)/sizeof(v[0]);
-    for(int i=0;i<len-1;i++){
-        if(v[i]!=v[i+1]){
-            contagem+=1;
-            nAtual = v[i+1];
+int contarNumeros(int v[], int len){
+    int contar = 1;
+    int termoAtual = v[0];
+    
+    for(int i=1;i<len;i++){
+        if(termoAtual != v[i]){
+            contar++;
+            termoAtual = v[i];
         }
     }
-    return contagem*2;
+    return contar*2;
+}
+
+int RLE(int v[], int len){
+    int termosResposta = contarNumeros(v, len);
+    int vetorR[termosResposta];
+    int termoAtual = v[0];
+   
+    int contar = 0;
+    int j = 0;
+    for(int i=0;i<len;i++){
+        if(v[i] == termoAtual){
+            contar++;
+        }else{
+            vetorR[j] = termoAtual;
+            vetorR[j+1] = contar;
+            j += 2;
+            contar = 1;
+            termoAtual = v[i];
+        }
+
+    }
+    vetorR[j] = termoAtual;
+    vetorR[j+1] = contar;
+    for(int i=0;i<termosResposta;i++){
+        printf("%i ", vetorR[i]);
+    }
+    return 0;
 }
 
 int main(){
-    int v[] = {1,2,3,5,5,5,7,7,7,7,7,2,2,2,0,0,0,0,0,9,9,9,12,12,12,4,4};
-    int total = sizeof(v)/sizeof(v[0]);
-    int totalR = contagemRLE(v);
-    int vR[totalR];
-    int contagemAtual = 1;
-    int nAtual = v[0];
-    int j = 0;
-    for(int i=0;i<total-1;i++){
-        if(v[i] == v[i+1]){
-            contagemAtual+=1;
-        }else{
-            nAtual = v[i];
-            vR[j] == nAtual;
-            vR[j+1]
-            contagemAtual = 1;
 
-        }
-    }
+    int vetor[] = {1,2,3,5,5,5,7,7,7,7,7,2,2,2,0,0,0,0,0,9,9,9,12,12,12,4,4};
+    int len = sizeof(vetor)/sizeof(vetor[0]);
+    RLE(vetor,len);
 
+    return 0;
 }
